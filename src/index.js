14fcb2857b8572fb1d37b11a07aa42d68b61f526
PedrOliveiraM <pedropucmont@gmail.com>
2024-07-11 22:44:26 -0300

    feat: modals and back

--- src/index.js
+++ src/index.js
@@ -66,10 +66,29 @@ function showCatalog(products) {
 
     const sideMenu = document.getElementById('sideMenu');
     sideMenu.classList.add('hidden');
+    const modalCart = document.getElementById('myCart');
+    modalCart.classList.add('hidden');
+
+    fetchBannerData();
+    const carrossel = document.getElementById('carrossel');
+    carrossel.classList.remove('hidden');
   } catch (error) {
     console.error('Erro ao exibir catálogo:', error);
   }
 }
+
+const btnLogoHome = document.getElementById('btnLogoHome');
+btnLogoHome.addEventListener('click', () => {
+  fetch('http://localhost:4000/pictures/')
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      showCatalog(data);
+    })
+    .catch((error) => {
+      console.error('Erro ao buscar dados:', error);
+    });
+});
 
 const btnTodos = document.getElementById('btnTodos');
 btnTodos.addEventListener('click', () => {
@@ -209,7 +228,7 @@ function listCategorias(products) {
   btnCategory.forEach((btn) => {
     btn.addEventListener('click', (event) => {
       const categoria = event.currentTarget.dataset.item;
-      alterProcutsShow(categoria); // Verifique se a função está definida corretamente
+      alterProcutsShow(categoria);
     });
   });
 }
@@ -217,3 +236,214 @@ function listCategorias(products) {
 //Na hora que clicar no botao de caegoria,
 // esconder a grid principal
 // e mostrar a grid de categorias
+
+//READY: IR PARA O CARRINHO
+const btnGoToCart = document.getElementById('btnGoToCart');
+const btnIcoGoToCart = document.getElementById('btnIcoGoToCart');
+
+btnIcoGoToCart.addEventListener('click', () => {
+  showCart();
+});
+btnGoToCart.addEventListener('click', () => {
+  showCart();
+});
+
+function showCart() {
+  //esconder os elementos de catalogo e mostrar o de carrinho
+  const myCart = document.getElementById('myCart');
+  const main = document.getElementById('main');
+  const sideMenu = document.getElementById('sideMenu');
+  const carrossel = document.getElementById('carrossel');
+
+  carrossel.classList.add('hidden');
+  sideMenu.classList.add('hidden');
+  myCart.classList.remove('hidden');
+  main.classList.add('hidden');
+}
+
+const btnCloseCart = document.getElementById('btnCloseCart');
+btnCloseCart.addEventListener('click', () => {
+  closeCart();
+});
+
+function closeCart() {
+  //esconder os elementos de carrinho e mostrar o de catalogo
+  const myCart = document.getElementById('myCart');
+  const main = document.getElementById('main');
+
+  main.classList.remove('hidden');
+  myCart.classList.add('hidden');
+}
+
+// TODO: CONFIG DOS BOTES DE ADD E DIM:
+document.body.addEventListener('click', (event) => {
+  const target = event.target;
+
+  if (
+    target.classList.contains('btn-minus') ||
+    target.classList.contains('btn-plus')
+  ) {
+    const quantityInput = target
+      .closest('.quantidade')
+      .querySelector('input[name="quantidade"]');
+    let currentValue = parseInt(quantityInput.value);
+
+    if (target.classList.contains('btn-minus')) {
+      if (currentValue > 1) {
+        // Previne quantidade menor que 1
+        quantityInput.value = currentValue - 1;
+      }
+    } else if (target.classList.contains('btn-plus')) {
+      quantityInput.value = currentValue + 1;
+    }
+  }
+
+  if (target.id === 'btnAddCart') {
+    const product = target.closest('.container-item');
+    const productId = target.dataset.id;
+    const quantity = parseInt(
+      product.querySelector('input[name="quantidade"]').value
+    );
+
+    console.log('Adicionar ao carrinho:', productId, quantity);
+    gerarPedido(productId, quantity);
+  }
+});
+
+function verificarAtacado(quantidade, qtdMin) {
+  if (quantidade >= qtdMin) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+async function gerarPedido(id, quantidade) {
+  //buscar o produto pelo id :
+  console.log(id);
+  console.log(quantidade);
+
+  const response = await fetch(`http://localhost:4000/pictures/${id}`);
+  const pedido = await response.json();
+
+  console.log(pedido);
+  // container onde vai ficar
+  const containerDePedidos = document.getElementById('containerDePedidos');
+
+  // verificar se é atacado
+  const valor = verificarAtacado(quantidade, pedido.qtdMin)
+    ? pedido.retail
+    : pedido.wholesale;
+
+  // criando um pedido
+  const item = document.createElement('div');
+  item.innerHTML = `
+  <div class="Pedido-1 border-2 border-[#747474] rounded-lg h-36 flex md:p-2 md:h-auto items-center px-1"
+  >
+    <img
+      class="w-30 h-32 md:w-48 md:h-64 shrink-0 rounded-xl"
+      src="${pedido.src}"
+      alt="imagem do produto"
+    />
+
+    <div
+      class="Infor space-y-5 flex flex-col justify-center items-center w-full"
+    >
+      <h2 class="font-bold text-center">${pedido.title}</h2>
+      <div class="flex justify-between w-full">
+        <div class="md:text-lg flex flex-col w-full px-2">
+          <div class="flex justify-between">
+            <p class="font-semibold">Valor:</p>
+            <p id="valorDoPedido">R$ ${valor}</p>
+          </div>
+
+          <div class="flex justify-between">
+            <p class="font-semibold">Qtd:</p>
+            <div
+              class="quantidade bg-gray-300 rounded w-24 h-7 flex justify-center items-center"
+            >
+              <button
+                id="btnDelProduct"
+                class="btn-minus scroll-py-10 text-black font-semibold rounded-lg"
+              >
+                -
+              </button>
+              <input
+                class="ml-2 text-center w-12 h-10 bg-transparent outline-none font-bold"
+                type="number"
+                name="quantidade"
+                value="${quantidade}"
+              />
+              <button
+                id="btnAddProduct"
+                class="btn-plus py-1 text-black font-semibold rounded-lg"
+              >
+                +
+              </button>
+            </div>
+          </div>
+
+          <div class="flex justify-between">
+            <p class="font-semibold">Valor Total:</p>
+            <p id="valorDoPedido">R$ ${valor * quantidade}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>  
+  `;
+
+  containerDePedidos.appendChild(item);
+
+  showModalAddToCart();
+}
+
+function showModalAddToCart() {
+  const modalAddCart = document.getElementById('modalAddCart');
+  modalAddCart.classList.remove('hidden');
+  modalAddCart.classList.add('flex');
+
+  setTimeout(() => {
+    modalAddCart.classList.add('hidden');
+    modalAddCart.classList.remove('flex');
+  }, 2000); // 3000 ms = 3 seconds
+}
+
+const btnModalGoToCart = document.getElementById('btnModalGoToCart');
+const btnModalStayHome = document.getElementById('btnModalStayHome');
+
+btnModalGoToCart.addEventListener('click', modalBtnGoToCart);
+btnModalStayHome.addEventListener('click', modalBtnStay);
+
+// ir para carrinho
+function modalBtnGoToCart() {
+  const modalMyCart = document.getElementById('myCart');
+  const modalAddCart = document.getElementById('modalAddCart');
+  modalMyCart.classList.add('hidden');
+  modalAddCart.classList.add('hidden');
+  showCart();
+}
+
+// voltar
+
+function modalBtnStay() {
+  const modalAddCart = document.getElementById('btnModalStayHome');
+  modalAddCart.classList.add('hidden');
+}
+
+const btnFinalizarPedido = document.getElementById('btnFinalizarPedido');
+btnFinalizarPedido.addEventListener('click', () => {
+  finalizarPedido();
+});
+
+
+// verificar a mudança do valor e quantidade e atualizar o valor total
+
+
+
+function finalizarPedido() {
+  //gerar texto com os items
+  // api do zap
+  // redirecionar
+  alert('Pedido finalizado');
+}
