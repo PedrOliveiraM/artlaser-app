e5806a540354fbcb9364608468e5df0464209dad
PedrOliveiraM <pedropucmont@gmail.com>
2024-07-12 21:22:13 -0300

    feat: finish main

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
+function init() {
+  fetchAllProducts();
+  fetchBannerData();
+  fetchCategories();
+}
+
+// NOTE: INICIANDO O SISTEMA
+init();
+
+const btnSearchProduct = document.getElementById('btnSearchProduct');
+const inputSearchProduct = document.getElementById('inputSearchProduct');
+
+inputSearchProduct.addEventListener('keyup', (event) => {
+  if (event.key === 'Enter') {
+    fetchProductBySearh();
+  }
+});
+btnSearchProduct.addEventListener('click', () => {
+  fetchProductBySearh();
+});
+
+function fetchProductBySearh() {
+  const search = inputSearchProduct.value;
+  fetch(`http://localhost:4000/pictures/search/${search}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      showCatalog(data);
+    })
+    .catch((error) => {
+      console.error('Erro ao buscar dados:', error);
+    });
+}
+
+//READY: MENU COM A GRID DE ITEMS
+const menu = document.getElementById('menu');
+// READY: ABRIR MENU
+function openMenu() {
+  menu.classList.remove('hidden');
+}
+//READY: FECHAR MENU
+function closeMenu() {
+  menu.classList.add('hidden');
+}
+// READY: MOSTRAR CATALOGO
 function showCatalog(products) {
   try {
     const catalog = document.getElementById('gridContainer');
@@ -72,13 +117,16 @@ function showCatalog(products) {
     fetchBannerData();
     const carrossel = document.getElementById('carrossel');
     carrossel.classList.remove('hidden');
+
+    const main = document.getElementById('main');
+    main.classList.remove('hidden');
   } catch (error) {
     console.error('Erro ao exibir catálogo:', error);
   }
 }
 
-const btnLogoHome = document.getElementById('btnLogoHome');
-btnLogoHome.addEventListener('click', () => {
+// READY: BUSCAR TODOS OS PRODUTOS
+function fetchAllProducts() {
   fetch('http://localhost:4000/pictures/')
     .then((response) => response.json())
     .then((data) => {
@@ -88,115 +136,32 @@ btnLogoHome.addEventListener('click', () => {
     .catch((error) => {
       console.error('Erro ao buscar dados:', error);
     });
-});
-
-const btnTodos = document.getElementById('btnTodos');
-btnTodos.addEventListener('click', () => {
+}
+// READY: BUSCAR PRODUTOS POR CATEGORIA
+function fetchProductsByCategory(category) {
+  fetch(`http://localhost:4000/pictures/category/${category}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      showCatalog(data);
+    })
+    .catch((error) => {
+      console.error('Erro ao buscar dados:', error);
+    });
+}
+// READY: BUSCAR CATEGORIAS DE PRODUTOS : MENU LATERAL
+function fetchCategories() {
   fetch('http://localhost:4000/pictures/')
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      showCatalog(data);
-    })
-    .catch((error) => {
-      console.error('Erro ao buscar dados:', error);
-    });
-});
-
-fetch('http://localhost:4000/pictures/')
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-    showCatalog(data);
-    listCategorias(data);
-  })
-  .catch((error) => {
-    console.error('Erro ao buscar dados:', error);
-  });
-
-function alterProcutsShow(categoria) {
-  fetch(`http://localhost:4000/pictures/category/${categoria}`)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      showCatalog(data);
+      listCategorias(data);
     })
     .catch((error) => {
       console.error('Erro ao buscar dados:', error);
     });
 }
-
-//TODO: Implementar banners
-// Função para buscar os dados do banner
-
-function fetchBannerData() {
-  fetch('http://localhost:4000/banner/')
-    .then((response) => response.json())
-    .then((data) => {
-      console.log('Fetch banner', data);
-      createCarousel(data);
-    })
-    .catch((error) => {
-      console.error('Erro ao buscar dados:', error);
-    });
-}
-function createCarousel(banners) {
-  const bannersContainer = document.getElementById('banners');
-
-  banners.forEach((banner) => {
-    const item = document.createElement('div');
-    item.classList.add('carousel-item');
-    item.innerHTML = `
-      <img class="w-screen h-auto rounded-xl" src="${banner.src}" alt="${banner.imageName}" />
-    `;
-    bannersContainer.appendChild(item);
-  });
-
-  const carouselItems = document.querySelectorAll('.carousel-item');
-  const totalItems = carouselItems.length;
-  const itemWidth = carouselItems[0].clientWidth;
-
-  let currentIndex = 0;
-
-  function showSlide(index) {
-    const offset = -index * itemWidth;
-    bannersContainer.style.transform = `translateX(${offset}px)`;
-    currentIndex = index;
-  }
-
-  // Inicia o carrossel
-  showSlide(currentIndex);
-
-  // Intervalo para avançar automaticamente
-  setInterval(() => {
-    currentIndex = (currentIndex + 1) % totalItems;
-    showSlide(currentIndex);
-  }, 3000); // Intervalo de 3 segundos para trocar os slides
-}
-
-fetchBannerData();
-
-const btnSideMenu = document.getElementById('btnSideMenu');
-const sideMenu = document.getElementById('sideMenu');
-
-btnSideMenu.addEventListener('click', () => {
-  sideMenu.classList.toggle('hidden');
-});
-
-window.addEventListener('click', function (event) {
-  const modalSideMenu = document.getElementById('sideMenu');
-  if (event.target === modalSideMenu) {
-    modalSideMenu.classList.toggle('hidden');
-  }
-});
-
-//READY: formatar letra
-function capitalizeFirstLetter(string) {
-  if (!string) return ''; // Verifica se a string está vazia ou indefinida
-  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
-}
-
-// TODO: Implementar categorias de produtos
+// READY: Implementar categorias de produtos
 function listCategorias(products) {
   const categoriasContainer = document.getElementById('containerCategorias');
   const categoriasSet = new Set();
@@ -228,26 +193,109 @@ function listCategorias(products) {
   btnCategory.forEach((btn) => {
     btn.addEventListener('click', (event) => {
       const categoria = event.currentTarget.dataset.item;
-      alterProcutsShow(categoria);
+      fetchProductsByCategory(categoria);
     });
   });
 }
+// READY: BTN LOGO HOME : HEADER
+const btnLogoHome = document.getElementById('btnLogoHome');
+btnLogoHome.addEventListener('click', () => {
+  fetchAllProducts();
+});
 
-//Na hora que clicar no botao de caegoria,
-// esconder a grid principal
-// e mostrar a grid de categorias
+// READY: BTN TODOS OS ITENS : MENU LATERAL
+const btnTodos = document.getElementById('btnTodos');
+btnTodos.addEventListener('click', () => {
+  fetchAllProducts();
+});
+
+// READY: BUSCAR BANNERS
+function fetchBannerData() {
+  fetch('http://localhost:4000/banner/')
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('Fetch banner', data);
+      createCarousel(data);
+    })
+    .catch((error) => {
+      console.error('Erro ao buscar dados:', error);
+    });
+}
+// READY: CRIAR CARROSSEL DE BANNERS
+function createCarousel(banners) {
+  const bannersContainer = document.getElementById('banners');
+
+  banners.forEach((banner) => {
+    const item = document.createElement('div');
+    item.classList.add('carousel-item');
+    item.innerHTML = `
+      <img class="w-screen h-auto rounded-xl" src="${banner.src}" alt="${banner.imageName}" />
+    `;
+    bannersContainer.appendChild(item);
+  });
+
+  const carouselItems = document.querySelectorAll('.carousel-item');
+  const totalItems = carouselItems.length;
+  const itemWidth = carouselItems[0].clientWidth;
+
+  let currentIndex = 0;
+
+  function showSlide(index) {
+    const offset = -index * itemWidth;
+    bannersContainer.style.transform = `translateX(${offset}px)`;
+    currentIndex = index;
+  }
+
+  // Inicia o carrossel
+  showSlide(currentIndex);
+
+  // Intervalo para avançar automaticamente
+  setInterval(() => {
+    currentIndex = (currentIndex + 1) % totalItems;
+    showSlide(currentIndex);
+  }, 4000); // Intervalo de 3 segundos para trocar os slides
+}
+
+// READY: MENU LATERAL
+const btnSideMenu = document.getElementById('btnSideMenu');
+const sideMenu = document.getElementById('sideMenu');
+
+// READY: ABRIR & FECHAR MENU LATERAL
+function openCloseSideMenu() {
+  sideMenu.classList.toggle('hidden');
+}
+// READY: BTN MENU LATERAL
+btnSideMenu.addEventListener('click', () => {
+  openCloseSideMenu();
+});
+
+// READY: FECHAR MENU QUANDO CLICADO FORA
+window.addEventListener('click', function (event) {
+  const modalSideMenu = document.getElementById('sideMenu');
+  if (event.target === modalSideMenu) {
+    openCloseSideMenu();
+  }
+});
+
+//READY: FORMATAR LETRAS DE CATEGORIAS : MENU LATERAL
+function capitalizeFirstLetter(string) {
+  if (!string) return ''; // Verifica se a string está vazia ou indefinida
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
 
 //READY: IR PARA O CARRINHO
 const btnGoToCart = document.getElementById('btnGoToCart');
 const btnIcoGoToCart = document.getElementById('btnIcoGoToCart');
-
+// READY: BTN CARRINHO
 btnIcoGoToCart.addEventListener('click', () => {
   showCart();
 });
+// READY: BTN CARRINHO
 btnGoToCart.addEventListener('click', () => {
   showCart();
 });
 
+// READY: MOSTRAR CARRINHO
 function showCart() {
   //esconder os elementos de catalogo e mostrar o de carrinho
   const myCart = document.getElementById('myCart');
@@ -260,12 +308,14 @@ function showCart() {
   myCart.classList.remove('hidden');
   main.classList.add('hidden');
 }
-
-const btnCloseCart = document.getElementById('btnCloseCart');
+// READY: BTN FECHAR CARRINHO
+const btnCloseCart = document.getElementById('btnKeepBuy');
 btnCloseCart.addEventListener('click', () => {
+  init();
   closeCart();
 });
 
+// READY: FECHAR CARRINHO
 function closeCart() {
   //esconder os elementos de carrinho e mostrar o de catalogo
   const myCart = document.getElementById('myCart');
@@ -310,14 +360,63 @@ document.body.addEventListener('click', (event) => {
   }
 });
 
-function verificarAtacado(quantidade, qtdMin) {
-  if (quantidade >= qtdMin) {
-    return true;
+// NOTE: PEDIDOS
+// READY: ARMAZENAR PEDIDOS:
+const meusPedidos = [
+  {
+    id: '',
+    title: '',
+    quantidade: '',
+    valor: '',
+    valorTotal: '',
+  },
+];
+// READY: FUNÇÃO PARA VERIFICAR O VALOR DO PRODUTO
+async function verificarValorAtacado(id, quantidade) {
+  const response = await fetch(`http://localhost:4000/pictures/${id}`);
+  const pedido = await response.json();
+
+  console.log('VERIFICAR VALOR ATACADO: pedido', pedido);
+  if (quantidade >= pedido.qtdMin) {
+    console.log('pedido.wholesale', pedido.wholesale);
+    return pedido.wholesale;
   } else {
-    return false;
+    console.log('pedido.wholesale', pedido.wholesale);
+    return pedido.retail;
   }
 }
 
+// READY: ATUALIZAR PEDIDO
+async function uptadePedido(id, quantidade) {
+  for (let pedido of meusPedidos) {
+    if (pedido.id === id) {
+      const valor = await verificarValorAtacado(id, quantidade);
+      pedido.valor = valor;
+      pedido.quantidade = quantidade;
+      pedido.valorTotal = parseFloat(pedido.valor * quantidade).toFixed(2);
+    }
+  }
+  uptadeTotal(); // Chame uptadeTotal após atualizar o pedido
+}
+
+// READY: ATUALIZAR TOTAL DO CARRINHO
+function uptadeTotal() {
+  const qtdeFinalItens = document.getElementById('qtdeFinalItens');
+  const valorFinalItens = document.getElementById('valorFinalItens');
+  let sumQtde = 0;
+  let sumValor = 0;
+
+  meusPedidos.forEach((pedido) => {
+    sumQtde += Number(pedido.quantidade);
+    sumValor += Number(pedido.valorTotal);
+  });
+  console.log(sumValor);
+  qtdeFinalItens.innerText = sumQtde;
+  valorFinalItens.innerText = `R$ ${sumValor.toFixed(2)}`;
+  console.log(sumValor);
+}
+
+// READY: GERAR PEDIDO
 async function gerarPedido(id, quantidade) {
   //buscar o produto pelo id :
   console.log(id);
@@ -330,14 +429,43 @@ async function gerarPedido(id, quantidade) {
   // container onde vai ficar
   const containerDePedidos = document.getElementById('containerDePedidos');
 
-  // verificar se é atacado
-  const valor = verificarAtacado(quantidade, pedido.qtdMin)
-    ? pedido.retail
-    : pedido.wholesale;
+  const valor = await verificarValorAtacado(id, quantidade);
 
-  // criando um pedido
-  const item = document.createElement('div');
-  item.innerHTML = `
+  if (meusPedidos.includes(pedido._id)) {
+    meusPedidos.forEach((pedido) => {
+      if (pedido.id === id) {
+        pedido.quantidade += quantidade;
+        pedido.valorTotal = pedido.quantidade * pedido.valor;
+      }
+    });
+    uptadeTotal();
+    return;
+  }
+
+  let existPedido = meusPedidos.find((pedido) => pedido.id === id);
+
+  if (existPedido) {
+    meusPedidos.forEach((pedido) => {
+      if (pedido.id === id) {
+        pedido.quantidade += quantidade;
+        pedido.valorTotal = pedido.quantidade * pedido.valor;
+      }
+    });
+    const quantidadeCart = document.getElementById('quantidadeCart');
+    quantidadeCart.value = existPedido.quantidade;
+    uptadeTotal();
+    return;
+  } else {
+    meusPedidos.push({
+      id: pedido._id,
+      title: pedido.title,
+      quantidade: quantidade,
+      valor: valor,
+      valorTotal: valor * quantidade,
+    });
+    // criando um pedido
+    const item = document.createElement('div');
+    item.innerHTML = `
   <div class="Pedido-1 border-2 border-[#747474] rounded-lg h-36 flex md:p-2 md:h-auto items-center px-1"
   >
     <img
@@ -345,11 +473,19 @@ async function gerarPedido(id, quantidade) {
       src="${pedido.src}"
       alt="imagem do produto"
     />
-
+    <span id="idDoPedido" class="hidden">${pedido._id}</span>
     <div
       class="Infor space-y-5 flex flex-col justify-center items-center w-full"
     >
-      <h2 class="font-bold text-center">${pedido.title}</h2>
+    <div class="flex justify-between">
+    <h2 class="font-semibold  text-center">${pedido.title}</h2>
+    <button
+      class="btnRemove px-3 scroll-py-10 text-black font-semibold rounded-lg">
+      <i class="fa-solid fa-trash text-red-600"></i>
+    </button
+    </div>
+    </div>
+      
       <div class="flex justify-between w-full">
         <div class="md:text-lg flex flex-col w-full px-2">
           <div class="flex justify-between">
@@ -364,7 +500,7 @@ async function gerarPedido(id, quantidade) {
             >
               <button
                 id="btnDelProduct"
-                class="btn-minus scroll-py-10 text-black font-semibold rounded-lg"
+                class="btn btn-minus-rem scroll-py-10 text-black font-semibold rounded-lg"
               >
                 -
               </button>
@@ -372,11 +508,13 @@ async function gerarPedido(id, quantidade) {
                 class="ml-2 text-center w-12 h-10 bg-transparent outline-none font-bold"
                 type="number"
                 name="quantidade"
+                id="quantidadeCart"
+                min="0"
                 value="${quantidade}"
               />
               <button
                 id="btnAddProduct"
-                class="btn-plus py-1 text-black font-semibold rounded-lg"
+                class="btn btn-plus-add py-1 text-black font-semibold rounded-lg"
               >
                 +
               </button>
@@ -392,12 +530,51 @@ async function gerarPedido(id, quantidade) {
     </div>
   </div>  
   `;
+    containerDePedidos.appendChild(item);
+    const input = item.querySelector('input[name="quantidade"]');
+    input.addEventListener('change', (event) => {
+      const quantidade = parseInt(event.target.value);
+      uptadePedido(pedido._id, quantidade);
+    });
 
-  containerDePedidos.appendChild(item);
+    // READY: remover pedido
+    const btnRemove = document.querySelectorAll('.btnRemove');
+    btnRemove.forEach((btn) => {
+      btn.addEventListener('click', (event) => {
+        const id = btn
+          .closest('.Pedido-1')
+          .querySelector('#idDoPedido').innerText;
+        meusPedidos.forEach((pedido, index) => {
+          if (pedido.id === id) {
+            meusPedidos.splice(index, 1);
+            btn.closest('.Pedido-1').remove();
+            uptadeTotal();
+          }
+        });
+      });
+    });
 
+    const btnAddProduct = item.querySelector('.btn-plus-add');
+    const btnDelProduct = item.querySelector('.btn-minus-rem');
+    btnAddProduct.addEventListener('click', () => {
+      const currentValue = parseInt(input.value);
+      input.value = currentValue + 1;
+      uptadePedido(pedido._id, currentValue + 1);
+    });
+    btnDelProduct.addEventListener('click', () => {
+      const currentValue = parseInt(input.value);
+      if (currentValue > 1) {
+        input.value = currentValue - 1;
+        uptadePedido(pedido._id, currentValue - 1);
+      }
+    });
+  }
+
+  uptadeTotal();
   showModalAddToCart();
 }
 
+// READY: MOSTRAR MODAL DE ADICIONAR AO CARRINHO
 function showModalAddToCart() {
   const modalAddCart = document.getElementById('modalAddCart');
   modalAddCart.classList.remove('hidden');
@@ -415,7 +592,7 @@ const btnModalStayHome = document.getElementById('btnModalStayHome');
 btnModalGoToCart.addEventListener('click', modalBtnGoToCart);
 btnModalStayHome.addEventListener('click', modalBtnStay);
 
-// ir para carrinho
+// ir para o carrinho
 function modalBtnGoToCart() {
   const modalMyCart = document.getElementById('myCart');
   const modalAddCart = document.getElementById('modalAddCart');
@@ -425,10 +602,9 @@ function modalBtnGoToCart() {
 }
 
 // voltar
-
 function modalBtnStay() {
-  const modalAddCart = document.getElementById('btnModalStayHome');
-  modalAddCart.classList.add('hidden');
+  const modalCart = document.getElementById('modalAddCart');
+  modalCart.classList.add('hidden');
 }
 
 const btnFinalizarPedido = document.getElementById('btnFinalizarPedido');
@@ -436,14 +612,33 @@ btnFinalizarPedido.addEventListener('click', () => {
   finalizarPedido();
 });
 
-
 // verificar a mudança do valor e quantidade e atualizar o valor total
-
-
-
 function finalizarPedido() {
-  //gerar texto com os items
-  // api do zap
-  // redirecionar
-  alert('Pedido finalizado');
+  // Gerar texto com os itens
+  if (meusPedidos.length === 0) {
+    alert('Adicione itens ao carrinho');
+    return;
+  }
+
+  const items = meusPedidos
+    .map((pedido) => {
+      if (pedido.quantidade && pedido.title && pedido.valorTotal) {
+        return `${pedido.quantidade}x ${pedido.title} - R$ ${pedido.valorTotal}`;
+      }
+    })
+    .join('\n');
+
+  const valorFinal = document.getElementById('valorFinalItens').innerText;
+  const qtdeFinal = document.getElementById('qtdeFinalItens').innerText;
+
+  const msg = `*Pedido:*\n${items}\n\n*Total de itens:* ${qtdeFinal}\n*Valor total:* ${valorFinal}`;
+  const encodedMsg = encodeURIComponent(msg);
+  const phone = '5562998092832';
+  const url = `https://api.whatsapp.com/send?phone=${phone}&text=${encodedMsg}`;
+
+  // Debugging
+  console.log('URL gerada:', url);
+
+  // Abrir o link do WhatsApp em uma nova aba
+  window.open(url, '_self');
 }
